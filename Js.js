d8000367fee07c7b0a28153360c6bdefbe3e6a26
Federico.Ciceri <166989779+Phadda94@users.noreply.github.com>
2024-10-06 18:01:52 +0200

    Js fixed

--- Js.js
+++ Js.js
@@ -1,17 +1,13 @@
 let button = document.getElementById('btn');
 let headList = document.getElementById('menu-bar');
-let cvButton = document.getElementById('btn-box');
-
-
-
+let page = document.getElementById('content');
 
 function showHeader () {
     if (window.innerWidth < 960) {
         headList.style.display = 'inline';
         button.style.display = 'none';
-   }
+    }
 }
-
 
 function hideHeader () {
     if (window.innerWidth < 960) {
@@ -20,9 +16,21 @@ function hideHeader () {
     }
 }
 
+function showHeader2 () {
+    if (window.innerWidth >= 960) {
+        headList.style.display = '';
+        button.style.display = 'none';
+    } else {
+        button.style.display = '';  // Riappare il bottone quando la finestra è più stretta di 960px
+    }
+}
 
-button.addEventListener('click', showHeader);
-
+// Eventi per mouse su button e headList
+button.addEventListener('mouseenter', showHeader);
 headList.addEventListener('mouseleave', hideHeader);
 
+// Aggiungi un listener per la modifica della larghezza della finestra
+window.addEventListener('resize', showHeader2);
 
+// Esegui la funzione una volta al caricamento della pagina per verificare la larghezza iniziale
+showHeader2();
